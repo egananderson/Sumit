86e251c93dc2e28448b56d2927a5a16a3e0822f4
Egan Anderson <p.egan.anderson@gmail.com>
2016-11-12 02:27:00 -0700

    core location working

--- MySumitVC.swift
+++ MySumitVC.swift
@@ -9,19 +9,21 @@
 import UIKit
 import CoreLocation
 
-var locationManager = CLLocationManager();
-
-class MySumitVC: UIViewController {
+class MySumitVC: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var sumitButton: UIButton!
     @IBOutlet var latitudeLabel: UILabel!
     @IBOutlet var longitudeLabel: UILabel!
+    
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locationManager = CLLocationManager();
-        locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
-        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters; // 100 m
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = kCLDistanceFilterNone;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         locationManager.startUpdatingLocation();
     }
     
@@ -31,9 +33,20 @@ class MySumitVC: UIViewController {
     }
 
     @IBAction func sumitButtonTapped(_ sender: UIButton) {
-        latitudeLabel.text = "\(locationManager.location?.coordinate.latitude)"
-        longitudeLabel.text = "\(locationManager.location?.coordinate.longitude)"
+        latitudeLabel.text = "\(locationManager.location!.coordinate.latitude)"
+        longitudeLabel.text = "\(locationManager.location!.coordinate.longitude)"
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
