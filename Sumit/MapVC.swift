6ceff245d7817cff942dbb38243640dd1c584c7c
Egan Anderson <p.egan.anderson@gmail.com>
2016-11-12 09:09:35 -0700

    moved buttons to bottom

--- Sumit/MapVC.swift
+++ Sumit/MapVC.swift
@@ -9,11 +9,12 @@
 import UIKit
 import GoogleMaps
 
-class MapVC: UIViewController, GMSMapViewDelegate {
+class MapVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
     
     // MARK: Properties
     
     var mapView: GMSMapView?
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +29,10 @@ class MapVC: UIViewController, GMSMapViewDelegate {
     }
     
     override func loadView() {
-        let latitude = 40.794413
-        let longitude = -111.890705
+        loadLocation()
+        
+        let latitude = locationManager.location!.coordinate.latitude
+        let longitude = locationManager.location!.coordinate.longitude
         let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 10)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
@@ -71,26 +74,31 @@ class MapVC: UIViewController, GMSMapViewDelegate {
         
         let width = UIScreen.main.bounds.width
         let height = UIScreen.main.bounds.height
+        
         // make header container
-        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 80))
+        let headerView = UIView(frame: CGRect(x: 0, y: height - 80, width: width, height: 80))
         headerView.backgroundColor = UIColor(colorLiteralRed: 19/255, green: 154/255, blue: 228/255, alpha: 1.0)
         
-        let leaderButton = UIButton(frame: CGRect(x: 0 , y: 5, width: width/2, height: headerView.frame.height - 5))
-        leaderButton.addTarget(self, action: #selector(showLeaderboard), for: .touchUpInside)
-        leaderButton.setTitle("Leaderboard", for: .normal)
-        leaderButton.setTitleColor(UIColor.white, for: .normal)
-        leaderButton.titleLabel?.font = UIFont(name: "System", size: 22)
-        leaderButton.titleLabel?.textAlignment = .center
+        let iconHeight = headerView.frame.height - 30
         
-        let sumitButton = UIButton(frame: CGRect(x: width/2 , y: 5, width: width/2, height: headerView.frame.height - 5))
+        let leaderButton = UIButton(frame: CGRect(x: 3*width/15 - iconHeight , y: 15, width: iconHeight, height: iconHeight))
+        leaderButton.addTarget(self, action: #selector(showLeaderboard), for: .touchUpInside)
+        leaderButton.setImage(UIImage(named: "leaderboard.png"), for: .normal)
+        
+        let sumitButton = UIButton(frame: CGRect(x: 12*width/15 , y: 19, width: iconHeight*0.8, height: iconHeight*0.8))
         sumitButton.addTarget(self, action: #selector(showUserSumits), for: .touchUpInside)
-        sumitButton.setTitle("My Sumits", for: .normal)
-        sumitButton.setTitleColor(UIColor.white, for: .normal)
-        sumitButton.titleLabel?.font = UIFont(name: "System", size: 22)
-        sumitButton.titleLabel?.textAlignment = .center
+        sumitButton.setImage(UIImage(named: "trophy.png"), for: .normal)
         
         headerView.addSubview(leaderButton)
         headerView.addSubview(sumitButton)
+        
+        let centerIconHeight = 4*iconHeight/3
+        
+        let recordSumitButton = UIButton(frame: CGRect(x: width/2 - ((centerIconHeight)*1.7737/2) , y: 5, width: (centerIconHeight)*1.7737, height: centerIconHeight))
+        recordSumitButton.addTarget(self, action: #selector(showRecordSumit), for: .touchUpInside)
+        recordSumitButton.setImage(UIImage(named: "sumitfulltext.png"), for: .normal)
+        
+        headerView.addSubview(recordSumitButton)
         
         return headerView
     }
@@ -106,6 +114,24 @@ class MapVC: UIViewController, GMSMapViewDelegate {
         let mySumitsVC = MySumitVC()
         self.present(mySumitsVC, animated: true, completion: nil)
     }
+    
+    func showRecordSumit() {
+        loadLocation()
+        
+        let mapController = MapController.sharedInstance
+        
+        if let latitude = locationManager.location?.coordinate.latitude {
+            let longitude = locationManager.location!.coordinate.longitude
+            print("latitude = \(latitude)")
+            print("longitude = \(longitude)")
+            if let destination = mapController.findDestination(latitude: latitude, longitude: longitude) {
+                print(destination.title)
+            } else {
+                print("no destination")
+            }
+        }
+
+    }
 
     // MARK: Google Maps Delegate
     
@@ -118,5 +144,24 @@ class MapVC: UIViewController, GMSMapViewDelegate {
     
     func leftToRight() {
         
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
+    
+    func loadLocation() {
+        locationManager = CLLocationManager();
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = kCLDistanceFilterNone;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.startUpdatingLocation();
     }
 }
